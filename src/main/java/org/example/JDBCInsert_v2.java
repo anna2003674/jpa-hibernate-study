1547d823a69842d1d26a489dfbcf7151742a4a13
AnnaTeremizova <amura780@gmail.com>
2025-02-28 10:32:01 +0300

    Insert using the Statement interface

---
+++ src/main/java/org/example/JDBCInsert_v2.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.sql.*;
+
+public class JDBCInsert_v2 {
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/hibernate-demo";
+    static final String USER = "postgres";
+    static final String PWD = "postgres";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Student student = new Student("Loe", "Farrel", 8.4);
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PWD);
+            Statement statement = connection.createStatement();
+            String sqlQuery = "INSERT INTO students (name, surname, avg_grade) VALUES" +
+                "('" +
+                student.getName() +
+                "', '" +
+                student.getSurname() +
+                "'," +
+                student.getAvgGrade() +
+                ")";
+            String sqlQuery2 = "INSERT INTO students (name, surname, avg_grade) VALUES ('Anna','Teremizova',6.6)";
+            statement.executeUpdate(sqlQuery);
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
