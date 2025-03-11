de372f274a911cb5ef1e5ac438304b61510dc7fe
AnnaTeremizova <amura780@gmail.com>
2025-03-11 20:26:29 +0300

    Changing Entity data in a table

---
+++ src/main/java/org/example/hiber/HibernateEx3.java
@@ -0,0 +1,34 @@
+package org.example.hiber;
+
+import org.example.hiber.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateEx3 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+            .configure("hibernate.cfg.xml")
+            .addAnnotatedClass(Student.class)
+            .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        try {
+            transaction.begin();
+            Student student = session.get(Student.class, 1);
+            student.setAvgGrade(10.5);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            factory.close();
+        }
+    }
+}
